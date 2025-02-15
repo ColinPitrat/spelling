0fb149921307b613c9e95fa932a94d1d9112c327
Colin Pitrat <colin.pitrat@gmail.com>
2025-02-15 10:40:58 +0000

    Only download what's needed

--- images/download.sh
+++ images/download.sh
@@ -1,4 +1,8 @@
 for img in $(grep jpg images.json | cut -d '"' -f 4 | sed 's/.jpg//')
 do
+  if [ -f "${img}.jpg" ]
+  then
+    continue
+  fi
   wget "https://ids.si.edu/ids/deliveryService?max_w=550&id=${img}" -O ${img}.jpg
 done
